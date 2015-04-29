b51ebebede641e9790986c961931b76d9faa4b1d
zackma <804532840@qq.com>
2015-04-29 18:05:30 +0800

    add

--- js/zackpop.js
+++ js/zackpop.js
@@ -3,6 +3,10 @@
  *author:zackma
  */
 
+if (typeof jQuery === 'undefined') {
+    throw new Error('zackpop requires jQuery');
+}
+
 var flag = false;   //Set a flag for function zackpop.init(). 
 
 //Define a css object where you can also add more object members(css) for your popup.
@@ -32,6 +36,14 @@ if(!flag){
     //Define musk for all popup here :
     var musk = '<div id = "musk"></div>';
 
+    //Define msgFram here :
+    var msgFram = '<div id = "msgFram">\
+                       <ul>\
+                           <li class = "msg"></li>\
+                           <li class = "btn"><span class="doOk">确 定</sapn></li>\
+                       </ul>\
+                  </div>';
+
     //Some constant for popup position :
     var dist = ($(window).width() - 280)/2 +'px';
     var docHeight = $(document).height() + 'px';
@@ -40,39 +52,35 @@ if(!flag){
     $.fn.extend({          
         
         alert:function(str){
-            
-            //Define msgFram here :
-            var msgFram = '<div id = "msgFram">\
-                               <ul>\
-                                   <li class = "msg">'+str+'</li>\
-                                   <li class = "btn"><span class="doOk">确 定</sapn></li>\
-                               </ul>\
-                          </div>';
-                                
+                                          
             //Show popup :
             if($('#musk,#msgFram').length == 0){      //Make sure there is no popup before executing append.
                 $('body').append(musk + msgFram);  
+                $('.msg').html(str);
 
                 //Set postion of msgFram and make musk cover the whole screen.     
                 $("#msgFram").css({"left":dist,"top":framTop});                        
                 $("#musk").css({"height":docHeight});
             }               
 
-            //Click the 'OK' button to remove popup.
-            $('.doOk').on('click',function(){
-                $('#musk,#msgFram').remove();
-            });
-
-            //Knock 'Enter' key to remove popup.
-            $(window).keydown(function(e){
-                if(e.keyCode == '13'){
+            (function(){
+                //Click the 'OK' button and musk to remove popup.
+                $('.doOk,#musk').on('click',function(){
                     $('#musk,#msgFram').remove();
-                }
-                return false;
-            });
+                });
+
+                //Knock 'Enter' key to remove popup.
+                $(window).keydown(function(e){
+                    if(e.keyCode == '13'){
+                        $('#musk,#msgFram').remove();
+                    }
+                }); 
+            })()
 
             $('#msgFram').focus();
             $(window).click(function(){return false;});
-        }              
+        },         
+
+        confirm:function(str){},
     });
 })(jQuery);
